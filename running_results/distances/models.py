7523e252d113b5161c180e567b16d35a029e7a55
Andreas Dvorak <a_github@dvorak.xyz>
2020-11-06 18:24:17 +0100

    add distance

---
+++ running_results/distances/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+
+class Distances(models.Model):
+    category_choices = [('d', 'distance'), ('t', 'time')]
+    sort = models.IntegerField(unique=True)
+    min = models.IntegerField()
+    max = models.IntegerField()
+    name = models.CharField(max_length=40)
+    category = models.CharField(choices=category_choices, max_length=1)
+
+    # order first for sort and than name
+    class Meta:
+        ordering = ("sort", "name")
--- running_results/running_results/settings.py
+++ running_results/running_results/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'fontawesome',
 
     # own
+    'distances',
     'events',
     'member',
     'pages',
